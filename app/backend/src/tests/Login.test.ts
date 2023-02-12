b91d4ddb5b82de51e3f93952b5929bdb84bb1f25
rafhaelgomes-dev <rafhaelgomes.dev@gmail.com>
2023-02-12 19:34:30 -0300

    Testes de integração

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -15,8 +15,8 @@ const { expect } = chai;
 describe('Testando rota de login', () => {
 
   const MockUser = {
-    username: 'rafhaelgomes@gmail.com',
-    password: 'asdasdasdasdasddasdasdas'
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
   }
 
   before(async () => {
@@ -29,9 +29,15 @@ describe('Testando rota de login', () => {
     (ModelUser.findOne as sinon.SinonStub).restore();
   })
 
-  it('Testando se é possivel realizar o login com sucesso e retornar um token', async () => {
-    const chaiHttpResponse: Response = await chai.request(app).post('/login').send({ username: 'rafhaelgomes@gmail.com', password: '123456'});
+  it('É possivel realizar o login com sucesso e retornar um token', async () => {
+    const chaiHttpResponse: Response = await chai.request(app).post('/login').send({ email: 'user@user.com', password: 'secret_user'});
     expect(chaiHttpResponse.status).to.be.eq(200);
-    expect(chaiHttpResponse.body).to.be.eq({ token: 'asdasdasdasdasddasdasdas' });
+    expect(chaiHttpResponse.body).to.have.all.keys('token');
+  });
+
+  it('Não é possivel fazer o login se não enviar um e-mail', async () => {
+    const chaiHttpResponse: Response = await chai.request(app).post('/login').send({ password: 'secret_user'});
+    expect(chaiHttpResponse.status).to.be.eq(400);
+    expect(chaiHttpResponse.body).to.deep.equal({ message: 'All fields must be filled' });
   });
 });
